b26fb3add64cc5caead1ea60a2b2178af3e13204
Piyush Narwal <111660644+PiyushDev9000@users.noreply.github.com>
2023-07-17 01:29:46 +0530

    Update page.jsx

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -3,9 +3,14 @@ import styles from "./page.module.css";
 import Image from "next/image";
 import Button from "@/components/Button/Button";
 
+export const metadata = {
+  title: "Piyush Narwal Contact Information",
+  description: "This is Contact Page",
+};
+
 const Contact = () => {
   return (
-    <div className={styles.Container}>
+    <div className={styles.container}>
       <h1 className={styles.title}>Let's Keep in Touch</h1>
       <div className={styles.content}>
         <div className={styles.imgContainer}>
@@ -16,17 +21,17 @@ const Contact = () => {
             className={styles.image}
           />
         </div>
-        <div className={styles.formContainer}>
+        <form className={styles.form}>
           <input type="text" placeholder="name" className={styles.input} />
           <input type="text" placeholder="email" className={styles.input} />
           <textarea
-            cols="30"
+            className={styles.textArea}
             placeholder="message"
+            cols="30"
             rows="10"
-            className={styles.textarea}
           ></textarea>
           <Button url="#" text="Send" />
-        </div>
+        </form>
       </div>
     </div>
   );
